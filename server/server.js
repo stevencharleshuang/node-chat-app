94fd5c9120746863519423d227eee644c7f7f065
Steven Huang <steven.charles.huang@gmail.com>
2018-06-16 23:47:26 -0400

    deployed to heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = process.env.PORT || 3000;
+const express   = require('express');
+const socketIO  = require('socket.io');
+const path      = require('path');
+
+const app       = express();
+const PORT      = process.env.PORT || 3000;
+
 
 const publicPath = path.join(__dirname, '../public');
 
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
 
 app.listen(PORT, () => {
-  console.log(`Server is up port: ${PORT} env: ${process.env}`);
-})
+  console.log(`Server is up port: ${PORT} env: ${app.get('env')}`);
+});
