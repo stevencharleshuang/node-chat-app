390f845b3d7e4c32ef2f8f27b7f74856e8d62ca4
Steven Huang <steven.charles.huang@gmail.com>
2018-06-17 00:08:42 -0400

    implemented socket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 const express   = require('express');
 const socketIO  = require('socket.io');
 const path      = require('path');
-
+const http      = require('http');
 const app       = express();
 const PORT      = process.env.PORT || 3000;
-
+const server    = http.createServer(app);
+const io        = socketIO(server);
 
 const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('>>> Server: New User Connected');
+
+  socket.on('disconnect', () => {
+    console.log('>>> Server: User was disconnected')
+  })
+});
+
+server.listen(PORT, () => {
   console.log(`Server is up port: ${PORT} env: ${app.get('env')}`);
 });
