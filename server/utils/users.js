89de3c55c742f7a21284ed279bc2f5b80a19241c
Steven Huang <steven.charles.huang@gmail.com>
2018-06-19 21:47:04 -0400

    added users class, not yet implemented, added users testing

--- server/utils/users.js
+++ server/utils/users.js
@@ -32,6 +32,21 @@ class Users {
     this.users.push(user);
     return user;
   }
+  removeUser(id) {
+    let user = this.getUser(id);
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+    return user;
+  }
+  getUser(id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+  getUserList(room) {
+    let users = this.users.filter((user) => user.room === room);
+    let namesArray = users.map((user) => user.name);
+    return namesArray;
+  }
 }
 
 module.exports = { Users };
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,6 +2,29 @@ const expect = require('expect');
 const { Users } = require('./users');
 
 describe('Users', () => {
+  let users;
+  beforeEach(() => {
+    users = new Users();
+    users.users = [
+    {
+      id: '1',
+      name: 'Mike',
+      room: 'testRoom'
+    },
+    {
+      id: '2',
+      name: 'Dan',
+      room: 'testRoom2'
+    },
+    {
+      id: '3',
+      name: 'Craig',
+      room: 'testRoom'
+    },
+    ]
+  });
+
+  // addUser()
   it('should add new user', () => {
     let users = new Users();
     let user = {
@@ -12,4 +35,47 @@ describe('Users', () => {
     let resUser = users.addUser(user.id, user.name, user.room);
     expect(users.users).toEqual([ user ]);
   });
+
+  // removeUser()
+  it('should remove a user', () => {
+    let userId = '1';
+    let user = users.removeUser(userId);
+
+    expect(user.id).toBe(userId);
+    expect(users.users.length).toBe(2);
+  });
+
+  it('should not remove a user', () => {
+    let userId = '99';
+    let user = users.removeUser(userId);
+
+    expect(user).toBeFalsy();
+    expect(users.users.length).toBe(3);
+  });
+
+  // getUser()
+  it('should find user', () => {
+    let userId = '3';
+    let user = users.getUser(userId);
+
+    expect(user.id).toBe(userId);
+  });
+
+  it('should not find user', () => {
+    let userId = '99';
+    let user = users.getUser(userId);
+
+    expect(user).toBeFalsy();
+  });
+
+  // getUserList()
+  it('should return names for testRoom', () => {
+    let userList = users.getUserList('testRoom');
+    expect(userList).toEqual(['Mike', 'Craig'])
+  });
+
+  it('should return names for testRoom2', () => {
+    let userList = users.getUserList('testRoom2');
+    expect(userList).toEqual(['Dan'])
+  })
 });
